4d0d7acdc872c596e9d2dd12dd1b6a96497b054b
Prof. M.K. Das <72753125+drmanab20@users.noreply.github.com>
2023-12-19 20:28:12 +0530

    Add files via upload

--- plot-v.sh
+++ plot-v.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #    set terminal postscript eps color enhanced
 #    set output "OF_vs_EXPT_T$i.eps"
-    set xlabel "Channel width, x / [m]"
+    set xlabel "Channel width x"
     set ylabel "v"
     set grid
+    set title "steady-state solution Ra=10e5"
     set key left top
     set size 0.6, 0.6
     set xrange [0:1]
-#    set yrange [285:310]
+    set yrange [-.5:.5]
     plot \
         "y0.5.xy" u ($1/0.05):($4/.119) w l, \
         "v-Ra-5.dat" u 1:2
